c589f47cd2ca43c8f66ae9b7793c87d1b10cf350
sokin-ili <sokiniliopoulos@gmail.com>
2024-10-07 00:55:25 +0300

    add script to create a Python virtual environment with required packages

---
+++ create_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python3 -m venv root2data
+
+. ./root2data/bin/activate # dot (.) is a synonym for source in bash.
+
+pip install --upgrade pip
+
+pip install pandas numpy awkward uproot h5py psycopg2-binary
+
+# execute the script by running the following command in the terminal: bash create_env.sh source create_env.sh
