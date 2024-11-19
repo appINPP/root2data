9ed244eba5840b69c9970bfe81bebf5b29e9ab72
sokin-ili <sokiniliopoulos@gmail.com>
2024-11-19 20:49:08 +0200

    modify file_ops.py  * add Apache License 2.0 header  * enhance docstrings format

--- utils/file_ops.py
+++ utils/file_ops.py
@@ -1,6 +1,19 @@
+#    Copyright 2024 appINPP
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+   
 # Purpose: Contains utility functions for file operations, such as listing HDF5 and SQLite files in a directory.
-# Key Functions: list_h5_files, list_sqlite_files.
- 
+
 
 import os
 from typing import List
@@ -11,10 +24,10 @@ def list_h5_files(h5_dir: str) -> List[str]:
     Lists all HDF5 files in the given directory.
 
     Parameters:
-     - h5_dir (str): Directory to search for HDF5 files.
+        - h5_dir (str): Directory to search for HDF5 files.
 
     Returns:
-     - List[str]: List of HDF5 file paths.
+        - List[str]: List of HDF5 file paths.
     
     Example:
     >>> list_h5_files("data/h5")
